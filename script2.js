8f7955ad4b57b3f79077aec2a0b7451ad9a8e4de
nobuyasu-t <nobuyasu.t@gmail.com>
2016-02-20 05:49:22 +0900

    Initial commit

---
+++ script2.js
@@ -0,0 +1,35 @@
+function EX_preloadImages() {
+    return;
+	var targetNode = document.getElementById('exlink');
+	var newNode1 = document.createElement('div');
+	newNode1.className = 'maincon  mgt10 textcenter';
+
+	var newNode21 = document.createElement('p');
+	newNode21.className = 'w96p mgl2p size18 bold borderbox';
+	var newNode31 = document.createElement('a');
+	newNode31.href = 'http://goo.gl/loSAOl'; //
+	var textNode31 = document.createTextNode('‹‹—¿‚ªã‚ª‚ç‚È‚¢‚±‚Æ‚ğƒNƒ\ãi‚Éƒuƒ`Ø‚ê‚½Œ‹‰Ê‚—‚—‚—¨‚Æ‚ñ‚Å‚à‚È‚¢Œ‹––‚Éc[PR]'); //
+	newNode1.appendChild(newNode21);
+	newNode21.appendChild(newNode31);
+	newNode31.appendChild(textNode31);
+	
+	var newNode22 = document.createElement('p');
+	newNode22.className = 'w96p mgl2p size18 bold borderbox';
+	var newNode32 = document.createElement('a');
+	newNode32.href = 'http://goo.gl/0DWycU'; //
+	var textNode32 = document.createTextNode('‚Q‚XÎAŒû‚Q‚R–œ‚Ì•‰‚¯‘gƒŠ[ƒ}ƒ“‚ª‘¦‚T–œ‰~‰Ò‚°‚½”é–§‚ğ–\˜I‚µ‚½Œ‹‰Ê‚—‚—‚—‚—[PR]'); //
+	newNode1.appendChild(newNode22);
+	newNode22.appendChild(newNode32);
+	newNode32.appendChild(textNode32);
+	
+	var newNode23 = document.createElement('p');
+	newNode23.className = 'w96p mgl2p size18 bold borderbox';
+	var newNode33 = document.createElement('a');
+	newNode33.href = 'http://tankiokanekasegu.seesaa.net/'; //
+	var textNode33 = document.createTextNode('’ZŠúŠÔ‚Å‚¨‹à‚ğ‰Ò‚®î•ñW'); //
+	newNode1.appendChild(newNode23);
+	newNode23.appendChild(newNode33);
+	newNode33.appendChild(textNode33);
+	
+	targetNode.parentNode.replaceChild(newNode1, targetNode);
+}
